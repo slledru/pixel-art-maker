079914611ff98f8caf06e2ac12beebd960c3a608
slledru <slledru@msn.com>
2018-02-17 07:36:33 -0700

    added newly selected color in front of the history

--- js/app.js
+++ js/app.js
@@ -1,8 +1,131 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const numberOfColors = 36;
+  const maxHistory = 10;
+  const colors = createColorPalette();
+  let selectedColor;
+  let inProgress = false;
+  let colorHistory = [];
+
   let canvas = document.getElementById('canvas');
-  for (let j = 0; j < 1440; j++) {
-    let col = document.createElement('div');
-    col.classList.add('pixel');
-    canvas.appendChild(col);
+  drawPaintArea(canvas);
+  canvas.addEventListener('click', dropColor);
+  canvas.addEventListener('mousemove', continuousDrawing);
+  canvas.addEventListener('mousedown', startDrawing);
+  canvas.addEventListener('mouseup', endDrawing);
+  let palette = document.getElementById('palette');
+  drawPaletteArea(palette);
+  palette.addEventListener('click', pickColor);
+  let chooser = document.getElementById('color-chooser');
+  chooser.addEventListener('input', selectColor);
+
+  function drawPaintArea(canvas) {
+    for (let j = 0; j < 1440; j++) {
+      let col = document.createElement('div');
+      col.classList.add('pixel');
+      canvas.appendChild(col);
+    }
+  }
+  function drawPaletteArea(palette) {
+    for (let j = 0; j < numberOfColors; j++) {
+      let col = document.createElement('div');
+      col.classList.add('color-palette');
+      col.style.backgroundColor = colors[j];
+      palette.appendChild(col);
+    }
+  }
+  function createColorPalette() {
+    let colors = [
+      '#DB2500',
+      '#FF5106',
+      '#FF8C6E',
+      '#FF0C00',
+      '#FFEE00',
+      '#DDBC9D',
+      '#35728A',
+      '#582627',
+      '#EA8746',
+      '#2EEDEC',
+      '#7B549A',
+      '#7B9A54',
+      '#281EFF',
+      '#4998E9',
+      '#7B7AFF',
+      '#C9AEC7',
+      '#9550F0',
+      '#10702E',
+      '#140079',
+      '#600080',
+      '#3001E2',
+      '#A035FF',
+      '#0B1675',
+      '#382681',
+      '#51FF06',
+      '#8CFF6E',
+      '#0CFF00',
+      '#EEFF00',
+      '#358A72',
+      '#631A09',
+      '#000000',
+      '#444444',
+      '#888888',
+      '#BBBBBB',
+      '#DDDDDD',
+      '#FFFFFF'
+    ];
+    return colors;
+  }
+  function pickColor(event) {
+    if (event.target.classList.contains('color-palette')) {
+      selectedColor = event.target.style.backgroundColor;
+      if (!colorHistory.includes(selectedColor)) {
+        let history = document.getElementById('history');
+        // colorHistory keeps maximum of 10 previously selected colors
+        // if colorHistory is full already, remove the oldest color
+        if (colorHistory.length >= maxHistory) {
+          colorHistory.pop();
+          history.removeChild(history.lastElementChild);
+        }
+        colorHistory.unshift(selectedColor);
+        // add the new div in front of the history
+        let color = document.createElement('div');
+        color.classList.add('color-palette');
+        color.style.marginTop = 0;
+        color.style.backgroundColor = selectedColor;
+        if (history.children.length > 0) {
+          history.insertBefore(color, history.children[0]);
+        } else {
+          history.appendChild(color);
+        }
+      }
+    }
+  }
+  function selectColor(event) {
+    selectedColor = event.target.value;
+  }
+  function dropColor(event) {
+    if (selectedColor !== undefined) {
+      if (event.target.classList.contains('pixel')) {
+        event.target.style.backgroundColor = selectedColor;
+      }
+    }
+  }
+  function continuousDrawing(event) {
+    if (selectedColor !== undefined) {
+      if (inProgress) {
+        if (event.target.classList.contains('pixel')) {
+          event.target.style.backgroundColor = selectedColor;
+        }
+      }
+    }
+  }
+  function startDrawing(event) {
+    if (event.target.classList.contains('pixel')) {
+      inProgress = true;
+    }
+  }
+  function endDrawing(event) {
+    if (event.target.classList.contains('pixel')) {
+      inProgress = false;
+    }
   }
 });
